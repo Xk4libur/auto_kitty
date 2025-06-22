1c02b874ecea352fb3b0573ec0358b447d992ac5
Xk4libur <xk4libur@gmail.com>
2025-06-22 16:20:05 +0100

    Update auto_kitty.py

--- auto_kitty.py
+++ auto_kitty.py
@@ -1,6 +1,7 @@
 #!/usr/python3
 
-import shutil 
+import shutil
+import os 
 import time
 from colorama import Fore, Style, init 
 init(autoreset=True)
@@ -37,4 +38,39 @@ if program_path:
     print(Fore.YELLOW + f"\n- {program_name} is already installed. :)\n")
 else:
     print(Fore.RED + f"\n- {program_name} is not installed. Install with 'sudo apt install kitty'\n")
+
+# Check if Fonts folder exists
+time.sleep(2)
+print(Fore.YELLOW + "\n[+] Checking if Fonts folder exists...\n")
+time.sleep(2)
+if not os.path.exists("/usr/share/fonts/"):
+    print(Fore.RED + "\n- Fonts folder does not exist. Creating...\n")
+    time.sleep(2)
+    os.makedirs("/usr/share/fonts/")
+    print(Fore.GREEN + "\n- Fonts folder created successfully. :)\n")
+else:
+    print(Fore.YELLOW + "\n- Fonts folder already exists. :)\n")
+
+# Move fonts to Fonts folder
+time.sleep(2)
+print(Fore.YELLOW + "\n[+] Moving fonts to Fonts folder...\n")
+time.sleep(2)
+
+hack_font_src = os.path.join(os.getcwd(), "Hack")
+hack_font_dst = "/usr/share/fonts/Hack"
+
+try:
+    if os.path.exists(hack_font_src):
+        if not os.path.exists(hack_font_dst):
+            shutil.move(hack_font_src, hack_font_dst)
+            print(Fore.GREEN + "\n- 'Hack' folder moved successfully. :)\n")
+        else:
+            print(Fore.YELLOW + "\n- 'Hack' folder already exists in /usr/share/fonts/.\n")
+    else:
+        print(Fore.RED + "\n- 'Hack' folder not found in the current directory.\n")
+except PermissionError:
+    print(Fore.RED + "\n- Permission denied. Try running the script with sudo.\n")
+except Exception as e:
+    print(Fore.RED + f"\n- Error moving 'Hack' folder: {e}\n")
+
 
