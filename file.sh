3775c9dbe85d0c1e6d278ac8cd34e67ed28fad0f
Xk4libur <xk4libur@gmail.com>
2025-06-22 21:30:20 +0100

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt install kitty zsh
-mv ~/auto_kitty/Hack /usr/share/fonts/
+sudo mv ~/auto_kitty/Hack /usr/share/fonts/
 mkdir -p ~/.config/kitty
-mv ~/auto_kitty/kitty.conf ~/.config/kitty/
-mv ~/auto_kitty/color.ini ~/.config/kitty/
+sudo mv ~/auto_kitty/kitty.conf ~/.config/kitty/
+sudo mv ~/auto_kitty/color.ini ~/.config/kitty/
