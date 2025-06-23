b654b4520787816ecc69fc4723f1149e13a3304f
Xk4libur <xk4libur@gmail.com>
2025-06-23 10:36:26 +0100

    z

--- file.sh
+++ file.sh
@@ -1,7 +1,76 @@
 #!/bin/bash
 
+cat << "EOF"
+    _         _          _    _ _   _         
+   / \  _   _| |_ ___   | | _(_) |_| |_ _   _ 
+  / _ \| | | | __/ _ \  | |/ / | __| __| | | |
+ / ___ \ |_| | || (_) | |   <| | |_| |_| |_| |
+/_/   \_\__,_|\__\___/  |_|\_\_|\__|\__|\__, |
+                                        |___/      made by xk4libur
+EOF
+
+
+# Check if kitty is installed
+
+kitty_checker() {
+    which kitty > /dev/null 2>&1
+
+    if [ $? -eq 0 ]; then
+        echo -e "Kitty is already installed"
+    else
+        echo -e "You must install kitty"
+        exit 1
+    fi
+}
+
+# Installing kitty
+
+echo -e "\nInstalling kitty...\n"
+sleep 2
+
+
+# Check if zsh is installed
+
+kitty_checker() {
+    which zsh > /dev/null 2>&1
+
+    if [ $? -eq 0 ]; then
+        echo -e "Zsh is already installed"
+    else
+        echo -e "You must install zsh"
+        exit 1
+    fi
+}
+
+# Installing zsh
+echo -e "\nInstalling zsh...\n"
+sleep 2
 sudo apt install kitty zsh
+
+# Move Hack Nerd Fonts
+echo -e "\nInstalling Hack Nerd Fonts...\n"
 sudo mv ~/auto_kitty/Hack /usr/share/fonts/
+sleep 2
+
+# Create configuration files
+echo -e "\nCreating the configuration files for kitty...\n"
 mkdir -p ~/.config/kitty
 sudo mv ~/auto_kitty/kitty.conf ~/.config/kitty/
 sudo mv ~/auto_kitty/color.ini ~/.config/kitty/
+sleep 2
+
+# Installing lsd and batcat
+echo -e "\nInstalling lsd and batcat...\n"
+sleep 2
+sudo dpkg -i bat.deb lsd.deb
+
+# Installing powerlevel10k
+echo -e "\nInstalling p10k...\n"
+sleep 2
+cd
+git clone https://github.com/romkatv/powerlevel10k.git
+zsh
+
+# Include batcat and lsd
+echo "alias ls='lsd -l' > ~/home/$USER/.zshrc"
+echo "alias cat='bat' > ~/home/$USER/.zshrc"
