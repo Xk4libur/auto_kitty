51647f3b32f3d8802b0bedb549aa4a0eee6c0cc5
Xk4libur <xk4libur@gmail.com>
2025-06-23 15:01:39 +0100

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cat << "EOF"
+set -e
+
+print_banner() {
+    cat << "EOF"
     _         _          _    _ _   _         
    / \  _   _| |_ ___   | | _(_) |_| |_ _   _ 
   / _ \| | | | __/ _ \  | |/ / | __| __| | | |
@@ -8,75 +11,95 @@ cat << "EOF"
 /_/   \_\__,_|\__\___/  |_|\_\_|\__|\__|\__, |
                                         |___/      made by xk4libur
 EOF
+    echo
+}
 
+check_kitty_installed() {
+    if ! command -v kitty >/dev/null 2>&1; then
+        echo -e "\n❌ Kitty no está instalado. Ejecuta:\n\n  sudo apt install kitty\n\ny luego vuelve a ejecutar este script desde Kitty.\n"
+        exit 1
+    fi
+}
 
-# Check if kitty is installed
-
-kitty_checker() {
-    which kitty > /dev/null 2>&1
-
-    if [ $? != 0 ]; then
-        echo -e "\nFist of all, install kitty (sudo apt install kitty) and then use it to do the rest.\n"
+ensure_using_kitty() {
+    if [ -z "$KITTY_WINDOW_ID" ]; then
+        echo -e "\n❌ Este script debe ejecutarse desde un terminal Kitty.\n"
         exit 1
     else
-        echo ""
+        echo -e "\n✅ Terminal Kitty detectado.\n"
     fi
 }
 
-use_kitty(){
-    if [ -n "$KITTY_WINDOW_ID" ]; then 
-        echo -e "\nYou are using a Kitty terminal\n"
-    else 
-        echo -e "\nUse a Kitty terminal to do the rest\n"
+install_zsh() {
+    echo -e "\n🔧 Instalando Zsh...\n"
+    sudo apt install -y zsh
+}
+
+install_fonts() {
+    echo -e "\n🔤 Instalando Hack Nerd Fonts...\n"
+    sudo mkdir -p /usr/share/fonts/Hack
+    sudo mv -f ~/auto_kitty/Hack/* /usr/share/fonts/Hack/
+    fc-cache -fv
+}
+
+configure_kitty() {
+    echo -e "\n⚙️  Configurando Kitty...\n"
+    mkdir -p ~/.config/kitty
+    mv -f ~/auto_kitty/kitty.conf ~/.config/kitty/
+    mv -f ~/auto_kitty/color.ini ~/.config/kitty/
+}
+
+install_powerlevel10k() {
+    echo -e "\n🌟 Instalando Powerlevel10k...\n"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+
+    if ! grep -q "powerlevel10k.zsh-theme" ~/.zshrc 2>/dev/null; then
+        echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+    fi
+
+    sudo chsh -s /usr/bin/zsh "$USER"
+    sudo chsh -s /usr/bin/zsh root
+
+    echo -e "\n🔧 Configurando Powerlevel10k para root...\n"
+    sudo bash -c '
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/powerlevel10k
+        echo "source ~/powerlevel10k/powerlevel10k.zsh-theme" >> /root/.zshrc
+    '
+}
+
+install_lsd_bat() {
+    echo -e "\n📦 Instalando lsd y bat...\n"
+    sudo dpkg -i bat.deb lsd.deb || {
+        echo -e "\n❌ Error al instalar los paquetes .deb. Verifica que existan y estén correctos.\n"
         exit 1
-    fi
+    }
 }
 
-kitty_checker
-use_kitty
+link_zshrc_to_root() {
+    echo -e "\n🔗 Enlazando configuración de zsh para root...\n"
+    sudo rm -f /root/.zshrc
+    sudo ln -s /home/"$USER"/.zshrc /root/.zshrc
+}
 
+add_aliases() {
+    echo -e "\n➕ Añadiendo alias a ~/.zshrc...\n"
+    {
+        echo "alias ls='lsd -l'"
+        echo "alias cat='bat'"
+    } >> ~/.zshrc
+}
 
-# Installing zsh
-echo -e "\nInstalling zsh...\n"
-sleep 2
-sudo apt install zsh -y 
+# -------------------- EJECUCIÓN --------------------
 
-# Move Hack Nerd Fonts
-echo -e "\nInstalling Hack Nerd Fonts...\n"
-sudo mv ~/auto_kitty/Hack /usr/share/fonts/
-sleep 2
+print_banner
+check_kitty_installed
+ensure_using_kitty
+install_zsh
+install_fonts
+configure_kitty
+install_powerlevel10k
+install_lsd_bat
+link_zshrc_to_root
+add_aliases
 
-# Create configuration files
-echo -e "\nCreating the configuration files for kitty...\n"
-mkdir -p ~/.config/kitty
-sudo mv ~/auto_kitty/kitty.conf ~/.config/kitty/
-sudo mv ~/auto_kitty/color.ini ~/.config/kitty/
-sleep 2
-
-# Installing powerlevel10k
-echo -e "\nInstalling p10k...\n"
-sleep 2
-cd
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-sudo chsh -s /usr/bin/zsh $USER
-sudo chsh -s /usr/bin/zsh root
-
-# Configurar Powerlevel10k para root
-sudo bash -c '
-cd /root
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo "source ~/powerlevel10k/powerlevel10k.zsh-theme" >> ~/.zshrc'
-
-# Installing lsd and batcat
-echo -e "\nInstalling lsd and batcat...\n"
-sleep 2
-sudo dpkg -i bat.deb lsd.deb
-
-# Symbolic link
-sudo rm -r /root/.zshrc
-sudo ln -s /home/$USER/.zshrc /root/.zshrc
-
-# Include batcat and lsd
-echo "alias ls='lsd -l' > /home/$USER/.zshrc"
-echo "alias cat='bat' > /home/$USER/.zshrc"
+echo -e "\n✅ Instalación y configuración completadas. Reinicia la terminal para aplicar los cambios.\n"
